8d44c60cdf55f7a590fc6d46f62f82dfd9ac531e
Frederico Neres <neresfrederico@gmail.com>
2021-07-08 09:37:51 -0300

    Lidando com erros retornados pela API gRPC

--- src/main/kotlin/br/com/zup/CalcularFreteController.kt
+++ src/main/kotlin/br/com/zup/CalcularFreteController.kt
@@ -1,8 +1,13 @@
 package br.com.zup
 
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
 
 @Controller
 class CalcularFreteController(val grpcClient: FretesServiceGrpc.FretesServiceBlockingStub) {
@@ -14,8 +19,33 @@ class CalcularFreteController(val grpcClient: FretesServiceGrpc.FretesServiceBlo
             .setCep(cep)
             .build()
 
-        val response = grpcClient.calculaFrete(request)
-        return CalcularFreteResponse(response)
+        try {
+            val response = grpcClient.calculaFrete(request)
+            return CalcularFreteResponse(response)
+
+        } catch (ex: StatusRuntimeException) {
+
+            val description = ex.status.description
+            val statusCode = ex.status.code
+
+            if(statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if(statusCode == Status.Code.PERMISSION_DENIED) {
+                val statusProto = StatusProto.fromThrowable(ex)
+                if(statusProto == null) {
+                    throw HttpStatusException(HttpStatus.FORBIDDEN, description)
+                }
+
+                val anyDetails = statusProto.detailsList.get(0)
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+                throw HttpStatusException(HttpStatus.FORBIDDEN,"${errorDetails.code} ${errorDetails.message}")
+
+            }
+
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.message)
+        }
     }
 }
 
